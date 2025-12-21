d8bb905682b297bd85625fb52443bfdbda794541
SimonRobotics <simonmm007@gmail.com>
2025-12-21 11:07:56 +0100

    first commit

---
+++ tank_control/tank_control/tank_control_node.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import serial
+import struct
+
+class CmdVelToSerial(Node):
+    def __init__(self):
+        super().__init__('tank_control_node')
+        self.subscription = self.create_subscription(
+            Twist,
+            'cmd_vel',
+            self.listener_callback,
+            10)
+        
+        self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+
+    def listener_callback(self, msg):
+        linear = msg.linear.x
+        angular = msg.angular.z
+
+        command = struct.pack('ff',linear, angular)
+        self.ser.write(command)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CmdVelToSerial()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
