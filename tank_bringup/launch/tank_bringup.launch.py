ea8da8cc8529415e33e758dff44b0f8a9c086a96
SimonRobotics <simonmm007@gmail.com>
2026-01-24 22:00:32 +0100

    launchfile add

---
+++ tank_bringup/launch/tank_bringup.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    # config_cam = os.path.join(
+    #     get_package_share_directory('tank_bringup'),
+    #     'config',
+    #     'camera_config.yaml'
+    # )
+
+    return LaunchDescription([
+        Node(
+            package='tank_control',
+            executable='tank_control',
+            name='tank_control',
+        ),
+
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+        ),
+
+        Node(
+            package='ros2_imu_reader',
+            executable='reader',
+        )
+
+
+    ])
